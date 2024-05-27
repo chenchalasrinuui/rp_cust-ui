a139fafd33b5795d995c904f479d262b95e68e4b
chenchalasrinuui <66296488+chenchalasrinuui@users.noreply.github.com>
2024-05-27 12:24:39 +0530

    latest

--- src/services/ajax.js
+++ src/services/ajax.js
@@ -1,18 +1,43 @@
 import axios from "axios";
 
-class Ajax {
 
-    sendGetReq(url) {
+// Add a request interceptor
+axios.interceptors.request.use(function (config) {
+    // Do something before request is sent
+    config.headers.Authorization=""
+    return config;
+}, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+});
+
+// Add a response interceptor
+axios.interceptors.response.use(function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    if(response.status===302){
+        
+    }
+    return response;
+}, function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+});
+
+export class Ajax {
+
+    static sendGetReq(url) {
         return axios.get(url)
     }
-    sendPostReq() {
-
+    static sendPostReq(url,data) {
+       return axios.post(url,data)
     }
 
-    sendPutReq() {
-
+    static sendPutReq(url,data) {
+       return axios.put(url,data)
     }
-    sendDeleteReq() {
-
+    static sendDeleteReq(url) {
+        return axios.delete(url)
     }
 }
